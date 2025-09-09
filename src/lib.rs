f9cc93f2f88228fe1286ed6f165fa55b7d3dc4ba
farooq13 <farukidris19@gmail.com>
2025-09-09 08:00:13 -0400

    add deposit struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,99 +1,65 @@
-use pinocchio::{account_info::AccountInfo, program_error::ProgramError, pubkey::find_program_address, ProgramResult};
+use pinocchio::{account_info::AccountInfo, instruction::Seed, program_error::ProgramError, pubkey::{find_program_address, Pubkey}, ProgramResult};
+use pinocchio::entrypoint;
 use pinocchio_system::instructions::Transfer;
+use pinocchio::nostd_panic_handler;
+
+
+entrypoint!(process_instruction);
+nostd_panic_handler!();
+
+pub mod instructions;
+pub use instructions::*;
+
+
+// 22222222222222222222222222222222222222222222
+pub const ID: Pubkey = [
+    0x0f, 0x1e, 0x6b, 0x14, 0x21, 0xc0, 0x4a, 0x07,
+    0x04, 0x31, 0x26, 0x5c, 0x19, 0xc5, 0xbb, 0xee,
+    0x19, 0x92, 0xba, 0xe8, 0xaf, 0xd1, 0xcd, 0x07,
+    0x8e, 0xf8, 0xaf, 0x70, 0x47, 0xdc, 0x11, 0xf7,
+];
+
+fn process_instruction(
+    _program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+) -> ProgramResult {
+    match instruction_data.split_first() {
+        Some((Deposit::DISCRIMINATOR, data)) => Deposit::try_from((data, accounts))?.process(),
+        Some((Withdraw::DISCRIMINATOR, _)) => Withdraw::try_from(accounts)?.process(),
+        _=> Err(ProgramError::InvalidInstructionData),
+    }
+}
 
 pub struct DepositAccounts<'a> {
     pub owner: &'a AccountInfo,
-    pub vault: &'a AccountInfo,
+    pub vault: &'a AccountInfo
 }
- 
+
 impl<'a> TryFrom<&'a [AccountInfo]> for DepositAccounts<'a> {
     type Error = ProgramError;
- 
+
     fn try_from(accounts: &'a [AccountInfo]) -> Result<Self, Self::Error> {
         let [owner, vault, _] = accounts else {
             return Err(ProgramError::NotEnoughAccountKeys);
         };
- 
+
         // Accounts Checks
         if !owner.is_signer() {
             return Err(ProgramError::InvalidAccountOwner);
         }
- 
+
         if !vault.is_owned_by(&pinocchio_system::ID) {
-            return Err(ProgramError::InvalidAccountOwner);
-        }
- 
-        if vault.lamports().ne(&0) {
             return Err(ProgramError::InvalidAccountData);
         }
- 
-        
-       
-        let (vault_key, _) = find_program_address(&[b"vault", owner.key()], &crate::ID);
+
+        let (vault_key, _) = find_program_address(&[b"vault", owner.key().as_ref()], &crate::ID);
         if vault.key().ne(&vault_key) {
             return Err(ProgramError::InvalidAccountOwner);
         }
- 
+
         // Return the accounts
-        Ok(Self { owner, vault })
-    }
-}
-
-pub struct DepositInstructionData {
-    pub amount: u64,
-}
-
-impl<'a> TryFrom<&'a [u8]> for DepositInstructionData {
-    type Error = ProgramError;
-
-    fn try_from(data: &'a [u8]) -> Result<Self, Self::Error> {
-        if data.len() != size_of::<u64>() {
-            return Err(ProgramError::InvalidInstructionData);
-        }
-
-        let amount = u64::from_le_bytes(data.try_into().unwrap());
-
-        // Instruction Checks
-        if amount.eq(&0) {
-            return Err(ProgramError::InvalidInstructionData);
-        }
-
-        Ok(Self { amount })
-    }
-}
-
-
-pub struct Deposit<'a> {
-    pub accounts: DepositAccounts<'a>,
-    pub instruction_data: DepositInstructionData,
-}
-
-impl<'a> TryFrom<(&'a [u8], &'a [AccountInfo])> for Deposit<'a> {
-    type Error = ProgramError;
-
-    fn try_from((data, accounts): (&'a [u8], &'a[AccountInfo])) -> Result<Self, Self::Error> {
-        let accounts = DepositAccounts::try_from(accounts)?;
-        let instruction_data = DepositInstructionData::try_from(data)?;
-
-        Ok(Self {
-            accounts,
-            instruction_data,
-        })
-    }
-}
-
-impl<'a> Deposit<'a> {
-    pub const DISCRIMINATOR: &'a u8 = &0;
-
-    pub fn process(&mut self) -> ProgramResult {
-        Transfer {
-            from: self.accounts.owner,
-            to: self.accounts.vault,
-            lamports: self.instruction_data.amount,
-        }
-        .invoke()?;
-
-        Ok(())
+        Ok(Self { owner, vault})
     }
 }
 
