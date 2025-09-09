a507aac516c834b52f2130312850e1ec9d91d299
farooq13 <farukidris19@gmail.com>
2025-09-09 08:21:27 -0400

    complete Deposit logic

--- src/lib.rs
+++ src/lib.rs
@@ -86,3 +86,37 @@ impl<'a> TryFrom<&'a [u8]> for DepositIntructionData {
         Ok(Self { amount })
     }
 }
+
+pub struct Deposit<'a> {
+    pub accounts: DepositAccounts<'a>,
+    pub instruction_data: DepositIntructionData,
+}
+
+impl<'a> TryFrom(&'a [u8], &'a [AccountInfo])> for Deposit<'a> {
+    type Error = ProgramError;
+
+    fn try_from((data, accounts)): (&'a [u8], &'a [AccountInfo]) -> Result<Self, Self::Error> {
+        let accounts = DepositAccounts::try_from(accounts)?;
+        let instruction_data = DepositIntructionData::try_from(data)?;
+
+        Ok(Self {
+            accounts,
+            instruction_data
+        })
+    }
+}
+
+impl<'a> Deposit<'a> {
+    pub const DISCRIMINATOR: &'a u8 = &0;
+
+    pub fn process(&mut self) -> ProgramResult {
+        Transfer {
+            from: self.accounts.owner,
+            to: self.accounts.vault,
+            lamports: self.instruction_data.amount
+        }
+        .invoke()?;
+
+        Ok(())
+    }
+}
