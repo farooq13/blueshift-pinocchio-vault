0fa63c3b9ab47c391a5c856a9d5cecbac9fe8de6
farooq13 <farukidris19@gmail.com>
2025-09-09 08:37:35 -0400

    implement withdraw

--- src/lib.rs
+++ src/lib.rs
@@ -155,3 +155,41 @@ impl<'a> TryFrom<&'a [AccountInfo]> for WithdrawAccounts<'a> {
         Ok(Self { owner, vault, bumps: [bump]})
     }
 }
+
+pub struct Withdraw<'a> {
+    pub accounts: WithdrawAccounts<'a>,
+}
+
+impl<'a> TryFrom<&'a [AccountInfo]> for Withdraw<'a> {
+    type Error = ProgramError;
+
+    fn try_from(accounts: &'a [AccountInfo]) -> Resultlll<Self, Self::Error> {
+        let accounts = WithdrawAccounts::try_from(accounts)?;
+
+        Ok(Self { accounts })
+    }
+}
+
+impl<'a> Withdraw<'a> {
+    pub const DISCRIMINATOR: &'a u8 = &1;
+
+    pub fn process(&mut self) -> ProgramResult {
+        // Create PDA signer seeds
+        let seeds = [
+            Seed::from(b"vault"),
+            Seed::from(self.accounts.owner.key().as_ref()),
+            Seed::from(&self.accounts.bumps),
+        ];
+        let signers = [Signer::from(&seeds)];
+
+        // Transfer all lamports from vault to owner
+        Transfer {
+            from: self.accounts.vault,
+            to: self.accounts.owner,
+            lamports: self.accounts.vault.lamports(),
+        }
+        .invoke_signed(&signers)?;
+        
+        Ok(())
+    }
+}
